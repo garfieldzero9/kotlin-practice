5573a5bdaa5362bcd09385d3d32d07790a98dce2
garfield09 <bayazid.nasir@mncbank.co.id>
2025-11-04 09:39:57 +0700

    feat: Add isPalindromeBigInt function and additional tests

--- app/src/main/kotlin/org/example/easy/PalindromeNumber.kt
+++ app/src/main/kotlin/org/example/easy/PalindromeNumber.kt
@@ -15,3 +15,19 @@ fun isPalindrome(x: Int): Boolean {
 
     return reversed == origin
 }
+
+fun isPalindromeBigInt(x: Int): Boolean {
+    if (x == 0) return true
+    if (x < 0 || x % 10 == 0) return false
+    var origin = x
+    var reversed = 0
+    var temp = x
+
+    while (temp > reversed) {
+        val digit = temp % 10
+        reversed = (reversed * 10) + digit
+        temp /= 10
+    }
+
+    return temp == reversed || temp == (reversed / 10)
+}
--- app/src/test/kotlin/org/example/easy/PalindromeNumberTest.kt
+++ app/src/test/kotlin/org/example/easy/PalindromeNumberTest.kt
@@ -25,4 +25,39 @@ class PalindromeNumberTest {
         val result = isPalindrome(input)
         Assertions.assertEquals(false, result)
     }
+
+    @Test
+    fun testPalindromeNumberWIthBigInteger() {
+        val input = 2147483647
+        val result = isPalindromeBigInt(input)
+        Assertions.assertEquals(false, result)
+    }
+
+    @Test
+    fun testPalindromeNumberWIthBigInteger1() {
+        val input = 12321
+        val result = isPalindromeBigInt(input)
+        Assertions.assertEquals(true, result)
+    }
+
+    @Test
+    fun testPalindromeNumberWIthBigInteger2() {
+        val input = 121
+        val result = isPalindromeBigInt(input)
+        Assertions.assertEquals(true, result)
+    }
+
+    @Test
+    fun testPalindromeNumberEdgeCase1() {
+        val input = 0
+        val result = isPalindrome(input)
+        Assertions.assertEquals(true, result)
+    }
+
+    @Test
+    fun testPalindromeNumberEdgeCase2() {
+        val input = 123443210
+        val result = isPalindrome(input)
+        Assertions.assertEquals(false, result)
+    }
 }
